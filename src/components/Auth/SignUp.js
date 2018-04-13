9f2a2fe7891668390384f6e41db07d13ea836c49
garretteklof <garrett.eklof@gmail.com>
2018-04-13 11:30:41 -0500

    style /welcome and authentication pages

--- src/components/Auth/SignUp.js
+++ src/components/Auth/SignUp.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import { login } from "../../actions/auth";
 import { callAddUser } from "../../api/users";
 
@@ -34,22 +35,48 @@ class SignUp extends React.Component {
   render() {
     const { email, password, error } = this.state;
     return (
-      <form className="signup" onSubmit={this.onSignUp}>
-        <input
-          className="signup__email"
-          type="email"
-          value={email}
-          onChange={this.onEmailChange}
-        />
-        <input
-          className="signup__password"
-          type="password"
-          value={password}
-          onChange={this.onPasswordChange}
-        />
-        <button className="signup__btn">Login</button>
-        {error && <span className="signup__error">{error}</span>}
-      </form>
+      <main className="auth">
+        <div className="auth__nav">
+          <Link to="/welcome">
+            <svg className="auth__backspace auth__backspace--signup">
+              <use href="/images/sprite.svg#icon-arrow-left-circle" />
+            </svg>
+          </Link>
+          <img src="/images/aliveberry.svg" />
+        </div>
+        <form className="auth__form" onSubmit={this.onSignUp}>
+          <div className="auth__form-group">
+            <input
+              className="auth__input auth__input--signup"
+              type="email"
+              placeholder="email"
+              value={email}
+              onChange={this.onEmailChange}
+              autoFocus
+            />
+            <label htmlFor="email" className="auth__label auth__label--signup">
+              Email
+            </label>
+          </div>
+          <div className="auth__form-group">
+            <input
+              className="auth__input auth__input--signup"
+              type="password"
+              placeholder="password"
+              value={password}
+              onChange={this.onPasswordChange}
+            />
+            <label
+              htmlFor="password"
+              className="auth__label auth__label--signup"
+            >
+              Password
+            </label>
+          </div>
+          <button className="btn--signup">Signup</button>
+          {error && <span className="auth__error">{error}</span>}
+        </form>
+      </main>
     );
   }
 }
